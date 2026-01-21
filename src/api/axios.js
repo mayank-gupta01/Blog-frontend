4e9d25936ac1c11cb43da3549fa1b86505908ed7
Mayank Gupta <mayank.gupta09022001@gmail.com>
2026-01-22 00:19:49 +0530

    update baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080" // change after deployment
+  baseURL: "https://blog-backend-bice-ten.vercel.app" // change after deployment
 });
 
 api.interceptors.request.use((config) => {
